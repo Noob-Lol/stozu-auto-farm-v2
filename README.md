77feb873a34451e26121385a09954370125e435b
Noob <111202059+Noob-Lol@users.noreply.github.com>
2024-07-06 14:07:39 +0300

    load extension from local profile

--- README.md
+++ README.md
@@ -3,22 +3,30 @@ Automated credit farm on [stozu](https://dash.stozu.net/), speed is ~5 per minut
 
 Made on Python with Selenium WebDriver.
 
-**This is Adsense version as Linkvertise got nuked.** XD
+**This is Adsense version as Linkvertise got nuked. XD**
 ## Update info:
 - Switched to regular Chrome, as I implemented everything without the need for a profile.
 - Unlimited speed*
 > [!TIP]
 > *You can open multiple cmd windows and run the sript in each, this will multiply the speed.
 >
-> 10x should earn about 50 credits per minute, which is faster than stozu-auto-farm. The maximum speed is limited by your CPU. 
+> 10x should earn about 50 credits per minute. The maximum speed is limited by your CPU. 
 - No more cloudflare, i added a proxy.
 > [!WARNING]  
 > Do NOT misuse or overload the proxy! I will shutdown it if someone does.
 >
-> You can only access *stozu.net websites with proxy.
+> You can only access stozu.net websites with proxy. Your IP's are logged, but I don't save them.
 # Requirements:
-- [Python](https://www.python.org/downloads/), selenium: ```pip install selenium```
+- [Python](https://www.python.org/downloads/)
+- Selenium: ```pip install selenium```
 - Google Chrome
+- [uBlock Origin](https://chromewebstore.google.com/detail/cjpalhdlnbpafiamejdnhcphjbkeiagm) in your default chrome profile.
 # How to use:
-- Download and unzip files in any folder.
+- Download the script.
 - Run script: ```py stozu_farm_ad.py```
+
+Guaranteed to work on Windows, other os - idk.
+# Contributing
+If you know how to improve something - make a pull request.
+## Made by
+n01b (discord), https://discord.gg/NoobNetwork
--- stozu_farm_ad.py
+++ stozu_farm_ad.py
@@ -10,16 +10,24 @@ from selenium.common.exceptions import NoSuchElementException, TimeoutException
 script_path = os.path.abspath(os.path.dirname(__file__))
 options=webdriver.ChromeOptions()
 options.add_argument('--disable-gpu')
-options.add_argument("--proxy-server=http://node1.stozu.net:6005")
 # yay stozu proxy!
-options.add_extension(f'{script_path}/uBlock-Origin.crx')
+options.add_argument("--proxy-server=http://node1.stozu.net:6005")
+extensions_path = os.path.expandvars('%LOCALAPPDATA%/Google/Chrome/User Data/Default/Extensions')
+ublock_path = os.path.join(extensions_path, 'cjpalhdlnbpafiamejdnhcphjbkeiagm')
+if os.path.isdir(ublock_path):
+    ublock_path = os.path.join(ublock_path, os.listdir(ublock_path)[0])
+else:
+    raise FileNotFoundError
+options.add_argument(f'--load-extension={ublock_path}')
 options.add_experimental_option('excludeSwitches', ['enable-automation'])
 
 ub_filters = '''!Noob filters, website##element
 dash.stozu.net##.elevation-4.sidebar-dark-primary.sidebar-open.main-sidebar
 dash.stozu.net##.navbar-light.navbar-dark.navbar-expand.navbar.sticky-top.main-header
 dash.stozu.net##.main-footer
-dash.stozu.net##.card-body > .row'''
+dash.stozu.net##.card-body > .row
+dash.stozu.net##.swal2-backdrop-show.swal2-top-end.swal2-container
+'''
 filters_enabled = False
 coin_count=0
 start_time=0.0
@@ -34,8 +42,6 @@ with webdriver.Chrome(options=options) as driver:
             except TimeoutException:
                 input('Timed out! Press Enter to start farm.')
                 print('Script started. Press Ctrl+C to exit.')
-            if start_time == 0.0:
-                start_time = time.time()
             driver.minimize_window()
             if filters_enabled is False:
                 driver.get("chrome-extension://cjpalhdlnbpafiamejdnhcphjbkeiagm/dashboard.html#1p-filters.html")
@@ -44,6 +50,8 @@ with webdriver.Chrome(options=options) as driver:
                 actions.send_keys(ub_filters).perform()
                 actions.key_down(Keys.CONTROL).send_keys("s").key_up(Keys.CONTROL).perform()
                 filters_enabled = True
+            if start_time == 0.0:
+                start_time = time.time()
             while True:
                 driver.get('https://dash.stozu.net/earn/ad')
                 time.sleep(10)
@@ -73,7 +81,10 @@ with webdriver.Chrome(options=options) as driver:
             if "disconnected: not connected to DevTools" in str(e):
                 print("Browser was closed. Exiting...")
                 break
-            print(f"An error occurred: {e}")
+            elif "net::ERR_PROXY_CONNECTION_FAILED" in str(e):
+                input("Proxy error! Press Enter to continue.")
+            else:
+                print(f"An error occurred: {e}")
             #input('Caught an error! Press Enter to restart.')
             print(f'Restarted with {coin_count} coins.')
             continue
@@ -82,4 +93,3 @@ with webdriver.Chrome(options=options) as driver:
     else:
         run_duration = time.time() - start_time
         print(f'Run time: {run_duration:.2f} seconds, earned {coin_count} coins.')
-    driver.quit()
