f2be3bf0d182d9f957a98b94850a0ac477b00fb6
Noob <111202059+Noob-Lol@users.noreply.github.com>
2024-07-08 17:03:54 +0300

    d
    +login with cookie option, code improvement

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ Automated credit farm on [stozu](https://dash.stozu.net/), speed is ~5 per minut
 Made on Python with Selenium WebDriver.
 
 **This is Adsense version as Linkvertise got nuked. XD**
-## Update info:
+## Update info
+- Added option to login with cookie as captcha was too hard.
 - Switched to regular Chrome, as I implemented everything without the need for a profile.
 - Unlimited speed*
 > [!TIP]
@@ -12,20 +13,25 @@ Made on Python with Selenium WebDriver.
 >
 > 10x should earn about 50 credits per minute. The maximum speed is limited by your CPU. 
 - No more cloudflare, i added a proxy.
-> [!WARNING]  
-> Do NOT misuse or overload the proxy! I will shutdown it if someone does.
->
-> You can only access stozu.net websites with proxy. Your IP's are logged, but I don't save them.
-# Requirements:
+- Read [proxy info](#proxy-info) for more.
+# Requirements
 - [Python](https://www.python.org/downloads/)
-- Selenium: ```pip install selenium```
+- Selenium, dotenv: ```pip install selenium```, ```pip install python-dotenv```
 - Google Chrome
 - [uBlock Origin](https://chromewebstore.google.com/detail/cjpalhdlnbpafiamejdnhcphjbkeiagm) in your default chrome profile.
-# How to use:
-- Download the script.
-- Run script: ```py stozu_farm_ad.py```
-
-Guaranteed to work on Windows, other os - idk.
+# How to use
+- Download and unzip the files.
+- Get "stozu_free_session" cookie from https://dash.stozu.net and add it in .env file.
+- To secure your cookies, do not share the contents of .env file with anyone.
+- Run script: ```py stozu_farm_ad.py cookie```
+- Guaranteed to work on Windows, other os - idk.
+## Proxy info
+> [!WARNING]  
+> Do NOT misuse or overload the proxy! I will shutdown it if someone does.
+- Logged: your IP. (cant do anything about it)
+- Uptime: 99.9%. 
+- Whitelisted sites: dash, free, status at stozu.net; some required sites for captcha.
+- If you have a site that should be whitelisted, please contact me somewhere...
 # Contributing
 If you know how to improve something - make a pull request.
 ## Made by
--- stozu_farm_ad.py
+++ stozu_farm_ad.py
@@ -1,4 +1,5 @@
-import time, os
+import time, os, sys
+from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
@@ -6,14 +7,23 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
-
+load_dotenv()
+# "py stozu_farm_ad.py <cookie variable name>" for this to work
+# where <cookie variable name> is the .env entry
+if len(sys.argv) < 2:
+    print("Cookie variable name not provided, login manually.")
+variable_name = sys.argv[1]
+cookie = os.environ.get(variable_name)
+if not cookie:
+    print(f"Variable '{variable_name}' not found")
+    sys.exit(1)
 script_path = os.path.abspath(os.path.dirname(__file__))
 options=webdriver.ChromeOptions()
 options.add_argument('--disable-gpu')
 # yay stozu proxy!
 options.add_argument("--proxy-server=http://node1.stozu.net:6005")
 extensions_path = os.path.expandvars('%LOCALAPPDATA%/Google/Chrome/User Data/Default/Extensions')
-ublock_path = os.path.join(extensions_path, 'cjpalhdlnbpafiamejdnhcphjbkeiagm')
+ublock_path = os.path.join(extensions_path,'cjpalhdlnbpafiamejdnhcphjbkeiagm')
 if os.path.isdir(ublock_path):
     ublock_path = os.path.join(ublock_path, os.listdir(ublock_path)[0])
 else:
@@ -21,36 +31,40 @@ else:
 options.add_argument(f'--load-extension={ublock_path}')
 options.add_experimental_option('excludeSwitches', ['enable-automation'])
 
-ub_filters = '''!Noob filters, website##element
+ub_filters = '''!Noob filters, website##.element
 dash.stozu.net##.elevation-4.sidebar-dark-primary.sidebar-open.main-sidebar
 dash.stozu.net##.navbar-light.navbar-dark.navbar-expand.navbar.sticky-top.main-header
 dash.stozu.net##.main-footer
 dash.stozu.net##.card-body > .row
 dash.stozu.net##.swal2-backdrop-show.swal2-top-end.swal2-container
 '''
-filters_enabled = False
+first_run = True
 coin_count=0
 start_time=0.0
 
 with webdriver.Chrome(options=options) as driver:
     while True:
         try:
-            driver.get('https://dash.stozu.net/home')
-            try:
-                WebDriverWait(driver, 120).until(EC.url_to_be('https://dash.stozu.net/home'))
-                print('Auto started! Press Ctrl+C to exit.')
-            except TimeoutException:
-                input('Timed out! Press Enter to start farm.')
-                print('Script started. Press Ctrl+C to exit.')
-            driver.minimize_window()
-            if filters_enabled is False:
+            if first_run:
+                first_run = False
+                driver.get('https://dash.stozu.net/home')
+                time.sleep(5)
+                if cookie:
+                    driver.add_cookie({"name": "stozu_free_session", "value": f"{cookie}"})
+                    print('Successfully logged in with cookie.')
+                else:
+                    try:
+                        WebDriverWait(driver, 120).until(EC.url_to_be('https://dash.stozu.net/home'))
+                        print('Auto started! Press Ctrl+C to exit.')
+                    except TimeoutException:
+                        input('Timed out! Press Enter to start farm.')
+                        print('Script started. Press Ctrl+C to exit.')
+                driver.minimize_window()
                 driver.get("chrome-extension://cjpalhdlnbpafiamejdnhcphjbkeiagm/dashboard.html#1p-filters.html")
                 time.sleep(1)
                 actions = ActionChains(driver)
                 actions.send_keys(ub_filters).perform()
                 actions.key_down(Keys.CONTROL).send_keys("s").key_up(Keys.CONTROL).perform()
-                filters_enabled = True
-            if start_time == 0.0:
                 start_time = time.time()
             while True:
                 driver.get('https://dash.stozu.net/earn/ad')
@@ -82,10 +96,14 @@ with webdriver.Chrome(options=options) as driver:
                 print("Browser was closed. Exiting...")
                 break
             elif "net::ERR_PROXY_CONNECTION_FAILED" in str(e):
-                input("Proxy error! Press Enter to continue.")
+                input("Proxy error! Press Enter to retry.")
+                continue
             else:
                 print(f"An error occurred: {e}")
             #input('Caught an error! Press Enter to restart.')
+            driver.switch_to.window(driver.window_handles[0])
+            driver.get('https://dash.stozu.net/earn/ad')
+            driver.minimize_window()
             print(f'Restarted with {coin_count} coins.')
             continue
     if start_time == 0.0:
